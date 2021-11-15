5a35ba3d6296a6db5042f3d1386f4f75001143a9
Deniz Ece Aktaş <aktasdeak@gmail.com>
2021-11-15 14:47:18 +0300

    Add files via upload

---
+++ logGame/Assets/Control.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+    GameObject text;
+    public bool isStart;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnMouseUp()
+    {
+        if (isStart)
+        {
+           
+
+            
+        }
+    }
+}
